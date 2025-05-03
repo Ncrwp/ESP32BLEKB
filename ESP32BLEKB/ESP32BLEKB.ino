02191e1d8f146b23e3ab1a760a50c49a635d8c37
NCRWP <Neilson@ncrwp.com>
2025-05-02 21:56:54 -0400

    UPDATE
    almost fully functional but I had to downgrade keyboard lib and sacrifice some code

--- ESP32BLEKB/ESP32BLEKB.ino
+++ ESP32BLEKB/ESP32BLEKB.ino
@@ -1,100 +1,54 @@
+#include <WiFi.h>
 #include <WebServer.h>
-#include <NimBLEDevice.h>
-#include <NimBLEHIDDevice.h>
-#include "wifi_connection.h"
-#include "hid_report.h"
-#include "config.h"
+#include <BleKeyboard.h>
+#include "config.h"  // Keep this for WiFi credentials
 
+// Minimal global variables
 WebServer server(80);
-WiFiConnection wifi;
-bool bleConnected = false;
-NimBLEHIDDevice* hid;
-NimBLECharacteristic* input;
+BleKeyboard bleKeyboard("BLE Keyboard");  // Give the keyboard a name (optional)
 
-class ServerCallbacks : public NimBLEServerCallbacks {
-    void onConnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo) {
-        bleConnected = true;
-        Serial.println("BLE connected");
-        pServer->updateConnParams(connInfo.getConnHandle(), 6, 12, 0, 400);
-    }
-    
-    void onDisconnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo, int reason) {
-        bleConnected = false;
-        Serial.println("BLE disconnected");
-        NimBLEDevice::startAdvertising();
-    }
-};
+// Ultra-minimal HTML (stored in PROGMEM)
+const char htmlPage[] PROGMEM = R"rawliteral(
+<html><body>
+<form action='/send' method='GET'>
+<input type='text' name='text' maxlength='50'><button>Send</button>
+</form></body></html>
+)rawliteral";
 
-void setupBLE() {
-    NimBLEDevice::init(DEVICE_NAME);
-    NimBLEDevice::setPower(BLE_POWER);
-    
-    NimBLEServer* serverBLE = NimBLEDevice::createServer();
-    serverBLE->setCallbacks(new ServerCallbacks());
-    
-    hid = new NimBLEHIDDevice(serverBLE);
-    
-    // Corrected manufacturer and HID info setup
-    hid->setManufacturer("ESP32");
-    hid->setHidInfo(0x00, 0x02);
-    hid->setReportMap((uint8_t*)reportMap, sizeof(reportMap));
-    hid->startServices();
-    
-    input = hid->getInputReport(1);
-    
-    NimBLEAdvertising* advertising = NimBLEDevice::getAdvertising();
-    advertising->addServiceUUID(hid->getHidService()->getUUID());
-    advertising->setAppearance(HID_APPEARANCE);
-    advertising->start();
+// Send text via BLE Keyboard (optimized loop)
+void sendText(String text) {
+  if (bleKeyboard.isConnected()) {
+    bleKeyboard.print(text);  // Send entire string at once (faster)
+  }
 }
 
-void sendKey(uint8_t keyCode) {
-    if (!bleConnected) {
-        Serial.println("Not connected - can't send key");
-        return;
-    }
-    
-    uint8_t report[8] = {0x00, 0x00, keyCode};
-    input->setValue(report, sizeof(report));
-    input->notify();
-    delay(10);
-    
-    memset(report, 0, sizeof(report));
-    input->setValue(report, sizeof(report));
-    input->notify();
+// Handle root request
+void handleRoot() {
+  server.send_P(200, "text/html", htmlPage);
 }
 
-void setupWebServer() {
-    server.on("/", []() {
-        String html = "<html><body><h2>ESP32 BLE Keyboard</h2>"
-                     "<p>Status: " + String(bleConnected ? "Connected" : "Disconnected") + "</p>"
-                     "<form action='/send'><button>Send 'A'</button></form>"
-                     "</body></html>";
-        server.send(200, "text/html", html);
-    });
-
-    server.on("/send", []() {
-        sendKey(0x04); // Send 'A' key
-        server.sendHeader("Location", "/");
-        server.send(303);
-    });
-
-    server.begin();
-    Serial.println("Web server started");
+// Handle send request
+void handleSend() {
+  if (server.hasArg("text")) {
+    sendText(server.arg("text"));
+  }
+  server.sendHeader("Location", "/");
+  server.send(303);
 }
 
 void setup() {
-    Serial.begin(115200);
-    
-    if (!wifi.connect()) {
-        delay(5000);
-        ESP.restart();
-    }
-    
-    setupBLE();
-    setupWebServer();
+  // Skip Serial.begin() to save space
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(100);
+
+  bleKeyboard.begin();  // Start BLE keyboard
+
+  // Minimal server setup
+  server.on("/", handleRoot);
+  server.on("/send", handleSend);
+  server.begin();
 }
 
 void loop() {
-    server.handleClient();
+  server.handleClient();
 }
